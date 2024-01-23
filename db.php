cafacd48f21ed5992a1ecee29eb3cdec3312d62f
MakVieSAinte <125467048+MakVieSAinte@users.noreply.github.com>
2024-01-23 13:53:05 +0100

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "authentification_chatgpt";
+$dbname = "authentification";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
